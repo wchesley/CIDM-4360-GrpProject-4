c32896e0a3fe8b5bd5901429665ef897bc3b0302
Walker Chesley <chesley.walker@gmail.com>
2019-11-07 10:24:25 -0600

    completed constructor
    I think the constructor is done? need to finish Invoice class first and double check this.
    added back setLineNumber

--- code-classes-relationship/InvoiceEntry.cs
+++ code-classes-relationship/InvoiceEntry.cs
@@ -5,14 +5,22 @@ namespace HW4
 {
     class InvoiceEntry
     {
-        public InvoiceEntry(List<Item> item)
+        ///<summary>
+        ///Use the new line number together with the passed (item) object 
+        ///and the requested quantity (ReqQuantity) to call the InvoiceEntry Constructor to create the new InvoiceEntry object.
+        ///</summary>
+        public InvoiceEntry(Item item, int lineNo, int ReqQuantity)
         {
-
+            LineNo = lineNo;
+            Qnty = ReqQuantity;
         }
         int LineNo; //a sequence number shows the line of the item in the invoice,
         int Qnty; // an int number shows the number of itemunits sold in that line
 
-
+        public void setLineNumber(int newLineNumber)
+        {
+            LineNo = newLineNumber; 
+        }
         public int getLineNumber()
         {
             return LineNo;
